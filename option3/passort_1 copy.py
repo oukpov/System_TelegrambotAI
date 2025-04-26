b8d0260730acdbe531a75dd09c6b6d0b75c6eba2
= <oukpovsiemreap@gmail.com>
2025-04-26 10:37:28 +0700

    pov

--- option3/passort_1 copy.py
+++ option3/passort_1 copy.py
@@ -7,25 +7,30 @@ import logging
 import re
 from datetime import datetime
 from telegram.ext import ContextTypes
-import unicodedata
-import random
+from flask import Flask, send_from_directory, request
+import threading
+
 # ========================= Configuration =========================
 
 BOT_TOKEN = "7669003420:AAGKhS6k8bTDxzNQR3_6cmnRPSkEgA8Xt0s"
 API_KEY = "AIzaSyAwuW-TTjKqYG7c-BSg_AquN37gv5Ia8OA"
-
+BASE_URL = "https://gtkn.up.railway.app"
+SAVE_FOLDER = 'image_option3'
 # ========================= Logging Setup =========================
-
+os.makedirs(SAVE_FOLDER, exist_ok=True)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # ========================= Folder Setup =========================
+flask_app = Flask(__name__)
 
-SAVE_FOLDER = "images"
-os.makedirs(SAVE_FOLDER, exist_ok=True)
 
-# Track processed images to avoid reprocessing
+@flask_app.route('/image_option3/<filename>')
+def serve_image(filename):
+    return send_from_directory(SAVE_FOLDER, filename)
+
+
 processed_files = set()
 
 
@@ -49,10 +54,13 @@ def information(
     group_name,
     gender,
     underline,
-    no
+    no,
+    image_url1,
+    image_blob
 
 ):
-
+    # print(
+    #     f'🌐 *************************************** {image_blob} 🌐 ***************************************')
     url = "https://oukpov.store/gtkn_project/public/api/optin3/add/data"
 
     payload = {
@@ -68,7 +76,9 @@ def information(
         "group_name": group_name,
         "gender": gender,
         "underline": underline,
-        "no": no
+        "no": no,
+        "url_1": image_url1,
+        "image_blob_1": image_blob
     }
 
     try:
@@ -89,6 +99,8 @@ def labor_Conference(
     account_name,
     account_type,
     account_no,
+    image_url2,
+    image_blob
 
 ):
     url = "https://oukpov.store/gtkn_project/public/api/add/Labor/conference"
@@ -98,6 +110,8 @@ def labor_Conference(
         "account_name": account_name,
         "account_type": account_type,
         "account_no": account_no,
+        "url_2": image_url2,
+        "image_blob_2": image_blob,
 
     }
 
@@ -165,17 +179,32 @@ async def photo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await process_images_by_index(context, message, context.user_data['photo_list'])
         context.user_data['photo_list'] = []
 
+# image_url1 = image_url2 = image_url3 = None
+
 
 async def process_images_by_index(context, message, photo_list):
     for index, photo in enumerate(photo_list):
-        file = await context.bot.get_file(photo.file_id)
         filename = f"{photo.file_unique_id}.jpg"
         file_path = os.path.join(SAVE_FOLDER, filename)
-        await file.download_to_drive(file_path)
+        # local_image_path = os.path.join(SAVE_FOLDER, filename)
+        if not os.path.exists(file_path):
+            telegram_file = await context.bot.get_file(photo.file_id)
+            await telegram_file.download_to_drive(file_path)
+            print(f"📥 Image downloaded to: {file_path}")
+        else:
+            print(f"✅ Image already exists: {file_path}")
+
+        image_url = f"{BASE_URL}/image_option3/{filename}"
+        path_image = f"/image_option3/{filename}"
+        # Convert to blob
+        with open(file_path, 'rb') as f:
+            image_blob = f.read()
+        encoded_image = base64.b64encode(image_blob).decode('utf-8')
 
         with open(file_path, 'rb') as image_file:
             content = base64.b64encode(image_file.read()).decode('utf-8')
 
+        # Google Vision OCR
         response = requests.post(
             f"https://vision.googleapis.com/v1/images:annotate?key={API_KEY}",
             headers={'Content-Type': 'application/json'},
@@ -190,6 +219,7 @@ async def process_images_by_index(context, message, photo_list):
         result = response.json()
         full_text = result['responses'][0].get(
             'fullTextAnnotation', {}).get('text', '')
+        # print(f"📝 OCR result from image {index + 1}:\n{full_text[:200]}...\n")
 
         def extract_field(pattern, fallback="N/A"):
             match = re.search(pattern, full_text, re.IGNORECASE)
@@ -198,28 +228,38 @@ async def process_images_by_index(context, message, photo_list):
         match index:
             case 0:
                 label = "Front"
+                image_url1 = image_url
+
                 if travel_doc_match:
                     passport_no = travel_doc_match.group(0)
-                    print(f'============> No.1 : {passport_no}')
-                    await passort_method(message, full_text, context)
+                    # print(
+                    #     f'============> No.1 : {passport_no}\n🖼image_url1 : {image_url1}')
+                    await passort_method(message, full_text, context, image_url1, encoded_image)
                 else:
                     passport_no = extract_field(
                         r'(?:លេខលិខិតឆ្លងដែន\s*/\s*Passport No\.?|Passport No\.?)[:\s]*([A-Z0-9]+)')
                     print(f'============> No.2 : {passport_no}')
-                    await passort_methodNo(message, full_text, context)
+                    await passort_methodNo(message, full_text, context, image_url1, encoded_image)
             case 1:
                 # label = "Back"
-
-                await labor_conference_Image(message, full_text, context, passport_no)
+                image_url2 = image_url
+                print(f"🧬 Image BLOB length: {len(image_blob)} bytes")
+                # print(
+                #     f'============> No.2 : {passport_no}\n🖼image_url2 : {image_url2}')
+                await labor_conference_Image(message, full_text, context, passport_no, image_url2, encoded_image)
             case 2:
+                image_url3 = image_url
+                print(f"🧬 Image BLOB length: {len(image_blob)} bytes")
+                # print(
+                #     f'============> No.3 : {passport_no}\n🖼image_url3 : {image_url3}')
                 # label = "Extra"
-                await calculate(message, context, passport_no)
+                await calculate(message, context, passport_no, image_url3, encoded_image)
             case _:
                 label = f"Image {index + 1}"
-        os.remove(file_path)
+        # os.remove(file_path)
 
 
-async def passort_method(message, full_text, context: ContextTypes.DEFAULT_TYPE):
+async def passort_method(message, full_text, context: ContextTypes.DEFAULT_TYPE, image_url1, image_blob):
     # sender = message.from_user
     chat_id = message.chat.id
     groupname = message.chat.title
@@ -315,11 +355,12 @@ async def passort_method(message, full_text, context: ContextTypes.DEFAULT_TYPE)
 
     information(
         surname, given_name, travel_doc_no, dob, doi, doe,
-        height, place_of_birth, chat_id, groupname, gender, mrz_1 + mrz_2, 1
+        height, place_of_birth, chat_id, groupname, gender, mrz_1 +
+        mrz_2, 1, image_url1, image_blob
     )
 
 
-async def passort_methodNo(message, full_text, context: ContextTypes.DEFAULT_TYPE):
+async def passort_methodNo(message, full_text, context: ContextTypes.DEFAULT_TYPE, image_url1, image_blob):
     chat_id = message.chat.id
     groupname = message.chat.title
     lines = full_text.strip().split('\n')
@@ -378,7 +419,8 @@ async def passort_methodNo(message, full_text, context: ContextTypes.DEFAULT_TYP
     # Save or forward data (call your internal method if needed)
     information(
         surname, given_name, passport_no, dob, doi, doe,
-        height, place_of_birth, chat_id, groupname, gender, mrz_1 + mrz_2, 2
+        height, place_of_birth, chat_id, groupname, gender, mrz_1 +
+        mrz_2, 2, image_url1, image_blob
     )
 
     # response_text = (
@@ -400,7 +442,7 @@ async def passort_methodNo(message, full_text, context: ContextTypes.DEFAULT_TYP
     # await context.bot.send_message(chat_id=chat_id, text=response_text)
 
 
-async def calculate(message, context: ContextTypes.DEFAULT_TYPE, passport_no):
+async def calculate(message, context: ContextTypes.DEFAULT_TYPE, passport_no, image_url3, image_blob):
     # === Sender info ===
     sender = message.from_user
     username = sender.username or sender.first_name
@@ -443,13 +485,15 @@ async def calculate(message, context: ContextTypes.DEFAULT_TYPE, passport_no):
         return
     if 'USD' in full_text or 'KHR' in full_text:
         # print(f'No.1 ====>')
-        extract_fields_Khmer(full_text, chat_id, 1, passport_no)
+        extract_fields_Khmer(full_text, chat_id, 1,
+                             passport_no, image_url3, image_blob)
     else:
         # print(f'No.2 ====>')
-        extract_field_thai(full_text, chat_id, 2, passport_no)
+        extract_field_thai(full_text, chat_id, 2,
+                           passport_no, image_url3, image_blob)
 
 
-def extract_fields_Khmer(text, groupID, bank_id, passport_no):
+def extract_fields_Khmer(text, groupID, bank_id, passport_no, image_url3, image_blob):
     amount_pattern = r'-?[\d,]+\.\d{2}\s*(KHR|USD)'
     amount_match = re.search(amount_pattern, text)
     named_line_pattern = r'^(?:Transfer to|Received from)\s+(.+)$'
@@ -481,7 +525,7 @@ def extract_fields_Khmer(text, groupID, bank_id, passport_no):
                             break
                     break
         calculateAmount(amount_clean,
-                        name, currency, groupID, bank_id, passport_no)
+                        name, currency, groupID, bank_id, passport_no, image_url3, image_blob)
         if name:
             print(f"✅ ==> Name only: {name}")
         else:
@@ -490,7 +534,7 @@ def extract_fields_Khmer(text, groupID, bank_id, passport_no):
         print("❌ No valid amount found.")
 
 
-def extract_field_thai(text, groupID, bank_id, passport_no):
+def extract_field_thai(text, groupID, bank_id, passport_no, image_url3, image_blob):
     matches = re.findall(r'\b\d+\.\d{2}\b', text)
 
     # Convert appropriately
@@ -507,15 +551,14 @@ def extract_field_thai(text, groupID, bank_id, passport_no):
     # print(f'===> Th : {amount}')
     # print(amount)
     calculateAmount(amount,
-                    "None", 'bat', groupID, bank_id, passport_no)
+                    "None", 'bat', groupID, bank_id, passport_no, image_url3, image_blob)
 
 
-def calculateAmount(amount, name, currency, group_id, bank_id, passport_no):
+def calculateAmount(amount, name, currency, group_id, bank_id, passport_no, image_url3, image_blob):
     if bank_id == 1:
         url = "calculate/amount/kh/option3"
     else:
         url = "calculate/amount/th/option3"
-        print(f'======> ({bank_id}) : 2')
 
     params = {
         "card_id": passport_no,
@@ -523,15 +566,19 @@ def calculateAmount(amount, name, currency, group_id, bank_id, passport_no):
         "amount": amount,
         "name": name,
         "currency": currency,
-        "group_id": group_id
-        # "group_name": group_name
+        "group_id": group_id,
+        "url_3": image_url3,
+        "image_blob_3": image_blob
     }
 
     try:
+        # Use json=data in POST request to send JSON payload
         response = requests.post(
-            f"https://oukpov.store/gtkn_project/public/api/{url}", params=params, json={})
+            f"https://oukpov.store/gtkn_project/public/api/{url}",
+            json=params  # Send data as JSON payload
+        )
         response.raise_for_status()  # Raises HTTPError for bad responses
-        labels_field = response.json()
+        labels_field = response.json()  # Parse JSON response
         return labels_field  # Return actual data
 
     except requests.exceptions.RequestException as e:
@@ -546,7 +593,7 @@ def format_date(date_str):
         return date_str
 
 
-async def labor_conference_Image(message, full_text, context: ContextTypes.DEFAULT_TYPE, card_id):
+async def labor_conference_Image(message, full_text, context: ContextTypes.DEFAULT_TYPE, card_id, image_url2, encoded_image):
     sender = message.from_user
     # username = sender.username or sender.first_name
     # user_id = sender.id
@@ -583,7 +630,8 @@ async def labor_conference_Image(message, full_text, context: ContextTypes.DEFAU
     #     f"📄 Account Type: {account_type}\n"
     #     f"🏬 Branch: {branch}"
     # )
-    labor_Conference(card_id, chat_id, name, account_type, account_no)
+    labor_Conference(card_id, chat_id, name,
+                     account_type, account_no, image_url2, encoded_image)
     # await message.reply_text(reply_text)
 
 
@@ -612,4 +660,6 @@ def main():
 
 
 if __name__ == "__main__":
+    threading.Thread(target=lambda: flask_app.run(
+        debug=True, use_reloader=False, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))).start()
     main()
